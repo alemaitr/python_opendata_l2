a631ad8eb5c4bbb3fc66ed6fce9faf1d61868758
Aurélie Lemaitre <aurelie.lemaitre@univ-rennes2.fr>
2024-09-30 15:13:04 +0200

    Reformulation 2024

--- TD5/Corrige/td5.py
+++ TD5/Corrige/td5.py
@@ -7,15 +7,12 @@ def acces_cle_api():
     data = json.load(fp)
     return data["GraphHopper"]
 
-
 #Exercice 1
-
 # Question 1
 def altitude(lieu1, gh_client):
     latlong_lieu1 = gh_client.address_to_latlong(lieu1)
     
     return gh_client.elevation_point(latlong_lieu1)
-
 
 # Question 2
 def les_altitudes(liste_gps, gh_client):
@@ -26,22 +23,18 @@ def les_altitudes(liste_gps, gh_client):
         alti.append(gh_client.elevation_point((lat, lng)))
     return alti
 
-
 # Exercice 2
 def deniveles(liste_gps, gh_client):
     liste_alti = les_altitudes(liste_gps, gh_client)
     deniv_pos = 0
     deniv_neg = 0
-
     for i in range(len(liste_alti) - 1):
         delta = liste_alti[i + 1] - liste_alti[i]
         if delta > 0:
             deniv_pos += delta
         else:
             deniv_neg += -delta
-
     return (deniv_pos, deniv_neg)
-
 
 # Exercice 3
 # Nb : il est nécessaire de convertir le format de coordonnées
@@ -51,7 +44,6 @@ def convertit_coords(lst_coords):
         lst2.append({"lat": coor["lat"], "lng": coor["lon"]})
     return lst2
 
-
 def les_randos(fichier, gh_client):
     fp = open(fichier, "r")
     les_randos = json.load(fp)
@@ -60,7 +52,6 @@ def les_randos(fichier, gh_client):
         coords = convertit_coords(rando["coords"])
         deniv = deniveles(coords, gh_client)
         print(f"{nom} \n \t - Dénivelé positif cumulé : {deniv[0]:.2f}\n \t - Dénivelé négatif cumulé : {deniv[1]:.2f}")
-
 
 # Exercice 4
 def ecrit_dico(fichier_in, gh_client, fichier_out):
@@ -78,13 +69,11 @@ def ecrit_dico(fichier_in, gh_client, fichier_out):
     fp2 = open(fichier_out, "w")
     json.dump(liste_finale,fp2,indent=2)
 
-
 #Début des tests
 os.chdir("TD5/Corrige")
 
 cle_api = acces_cle_api()
 gh_client = graphh.GraphHopper(api_key=cle_api)
-
 
 #Exercice 1 :altitude
 # Test Q1
