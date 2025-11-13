754fc99fd9ec072574ebc300de439e3222a0b51d
Aurelie Lemaitre <alemaitr@ad.inria.fr>
2025-11-13 15:17:59 +0100

    Amélioration du nouveau sujet TD9

--- TD9/corrige/td9.py
+++ TD9/corrige/td9.py
@@ -9,6 +9,7 @@ import csv
 from datetime import datetime
 from pprint import pprint
 from cartographie import *
+
 ##############################################
 # Fonctions 
 ##############################################
@@ -25,7 +26,7 @@ def charge_trouvailles():
         contenu = csv.DictReader(fp,delimiter=";")
         dic_fin = {}
         for dico in contenu :
-            date = datetime.strptime(dico["date_trouvaille"],"%d/%M/%Y")
+            date = datetime.strptime(dico["date_trouvaille"],"%d/%m/%Y")
             dic_fin[dico["id_tresor"]] = date
     return dic_fin
 
@@ -33,26 +34,12 @@ def enrichit_dico(tresors, trouvaille):
     for t in tresors :
         id = t["nid"]
         if id in trouvaille :
-            date = trouvaille[id]
-            t["trouvaille"] = date
+            t["trouvé"] = True
+            t["date"] = trouvaille[id]
         else :
-            t["trouvaille"] = None
+            t["trouvé"] = False
 
-def ecrit_mes_tresors(tresors,fichier):
-    lst = []
-    for tres in tresors :
-        dic = {}
-        dic["lat"] = tres["lat"]
-        dic["lng"] = tres["lng"]
-        dic["trouvaille"] = str(tres["trouvaille"])
-        dic["nom"] = tres["nom"]
-        dic["type"] = tres["type"]
-        lst.append(dic)
-
-    with open(fichier,"w",encoding="utf-8") as fp : 
-        json.dump(lst,fp,indent=2)
-    print(f"Fichier {fichier}  écrit avec succès.")
-
+## Exercice 2
 def analyse_donnees(tresors):
     resultat = {}
     for tres in tresors : 
@@ -62,22 +49,55 @@ def analyse_donnees(tresors):
         else :
             dico = {"nb":0,"trouvé": 0}
         dico["nb"] = dico["nb"]+1
-        if tres["trouvaille"]:
+        if tres["trouvé"]:
             dico["trouvé"] = dico["trouvé"]+1
         resultat[type]= dico
     return resultat
 
+
+def type_prefere(dico_type):
+    type_max = None
+    trouve_max = -1
+    for type, dico_nb in dico_type.items():
+        t= dico_nb["trouvé"]
+        if t> trouve_max:
+            trouve_max = t
+            type_max = type
+    return type_max
+
+def type_plus_avancé(dico_type):
+    type_max = None
+    pourc_max = -1
+    for type, dico_nb in dico_type.items():
+        p= dico_nb["trouvé"]/dico_nb["nb"]
+        if p> pourc_max:
+            pourc_max = p
+            type_max = type
+    return type_max
+
+#Exercice 3
 def affiche_trouvailles_ordonnees (tresors):
-    trouves = [t for t in tresors if t["trouvaille"]]
-    trouves.sort(key=lambda x: x["trouvaille"])
+    trouves = [t for t in tresors if t["trouvé"]]
+    trouves.sort(key=lambda x: x["date"])
     print("_____________________________________________")
     print("Liste des trouvailles par ordre de découverte")
     print("_____________________________________________")
     for t in trouves :
-        date = t['trouvaille'].strftime("%d/%M/%Y")
-        print(f"Le {date} : {t['nom']} à {t['ville']} ")
+        date = t['date'].strftime("%d/%m/%Y")
+        print(f"Le {date} à {t['ville']} : {t['nom']} ")
     print("_____________________________________________")
 
+def analyse_mois_trouvaille(trouvailles):
+    dico_mois = {m:0 for m in range(1,13)}
+    for date in trouvailles.values():
+        m = date.month
+        dico_mois[m]+=1
+    return dico_mois
+
+
+
+
+#Exercice 4
 def cartographier_trouvailles(tresors):
     carte = creer_carte("Terra Aventura")
     for tres in tresors :
@@ -98,26 +118,34 @@ os.chdir("TD9")
 
 tresors = charge_tresors()
 print(len(tresors))
-print(tresors[0])
+# print(tresors[0])
 
 trouvailles = charge_trouvailles()
 print(len(trouvailles))
-print(trouvailles)
+pprint(trouvailles)
 
 enrichit_dico(tresors, trouvailles)
 pprint(tresors[0])
 pprint(tresors[1])
-# ecrit_mes_tresors(tresors,"corrige/mes_tresors.json")
 
-# ## Exercice 2 : Analyse des données
+
+## Exercice 2 : Trésor de prédilection
 resultat = analyse_donnees(tresors)
 pprint(resultat)
 
-trouvailles = affiche_trouvailles_ordonnees(tresors)
+print(f"Le type de trésors favori de Marco et Polo est {type_prefere(resultat)}")
+# Le type de trésors favori de Marco et Polo est Zabeth
+print(f"Le type de trésors sont les trouvailles sont les plus avancées est {type_plus_avancé(resultat)}")
+# Le type de trésors sont les trouvailles sont les plus avancées est Ziraider
 
 
-# ## Exercice 3 : Cartographie
+## Exercice 3 : Ordre des découvertes
+affiche_trouvailles_ordonnees(tresors)
+mois = analyse_mois_trouvaille(trouvailles)
+print(mois)
 
-cartographier_trouvailles(carte,tresors)
+## Exercice 4 : Cartographie
+
+# cartographier_trouvailles(carte,tresors)
 
    
